ad32ca31f882bcd11c4ebde831a0ff33a4ae781c
Faiz <faiz@192.168.1.2>
2025-08-03 23:23:20 +0530

    directory setup

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <h1>Hello from the backend</h1>
+      <div>{message}</div>
+    </>
+  );
+}
+
+export default App;
