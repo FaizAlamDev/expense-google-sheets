4a8775d110f649002193d44df993f82870df3909
Faiz Alam <faizalam9883@gmail.com>
2025-08-17 23:00:51 +0530

    Home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,10 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:5000")
-      .then((res) => res.text())
-      .then((data) => setMessage(data))
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
     <>
-      <h1>Hello from the backend</h1>
-      <div>{message}</div>
+      <h1>Expense Logging App</h1>
+      <footer>
+        <p>Made by Faiz Alam</p>
+      </footer>
     </>
   );
 }
