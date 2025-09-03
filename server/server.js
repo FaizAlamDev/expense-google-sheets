a8fe0e06c8b8c0ff90d4a597e4e2b96d23b82d97
Faiz Alam <faizalam9883@gmail.com>
2025-09-03 23:27:21 +0530

    docker update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use("/", routes);
 
 initializeAuth()
   .then(() => {
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server listening on PORT ${PORT}`);
     });
